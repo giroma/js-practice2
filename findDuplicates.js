6c8e9479e03476061087b527812a026a253bac13
Argir Argirov <dj.giro@gmail.com>
2018-04-30 16:19:31 -0400

    clarifying problem

--- findDuplicates.js
+++ findDuplicates.js
@@ -1,3 +1,5 @@
+//Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array),
+//some elements appear twice and others appear once.
 //Find all the elements that appear twice in this array.
 var nums = [4,3,2,7,8,2,3,1]
 var findDuplicates = function(nums) {
