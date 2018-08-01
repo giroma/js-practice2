5615354f666636af6065f4bb6537dedfdee6c28b
Argir Argirov <dj.giro@gmail.com>
2018-08-01 15:30:34 -0400

    without shift

--- stackpop.js
+++ stackpop.js
@@ -1,28 +1,29 @@
-stack1 = []
+stack1 = [1,2,3,4,5]
 stack2 = []
 class Stack{
-  constructor(){
-
-  }
   static push(num){
     stack1.push(num)
   }
   static pop(){
-    let next = stack1[0]
-    stack1.shift()
-    stack2.push(next)
+    let size = this.peek()
+    for (let i = 1; i < size; i++) {
+      stack2.push(stack1.pop())
+    }
+    stack1.pop()
+    for (let i = 0; i < size-1; i++) {
+      stack1.push(stack2.pop())
+    }
+    console.log('final', stack1);
+    console.log('final', stack2);
   }
   static peek(){
-  console.log('stack1:', stack1)
-  console.log('stack2:', stack2)
+  return stack1.length + stack2.length
   }
 
 }
-Stack.push(1)
-Stack.push(2)
-Stack.push(3)
-Stack.peek()
-Stack.push(7)
-Stack.peek()
+
+// Stack.peek()
+Stack.push(99)
 Stack.pop()
-Stack.peek()
+Stack.pop()
+// Stack.peek()
